b780afaee683ccabcb9a1b1e083b9e256394cd6a
WOE-Y <876362620@qq.com>
2026-08-22 18:08:47 +0800

    Untrack recurrent KDA tests and correct docs

--- fla/ops/ascendc/kda/recurrent_kda/tests/operators/routes/test_aclnn_recurrent_kda.cpp
+++
@@ -1,8 +0,0 @@
-// Canonical aclnn route contract for RecurrentKda.
-// Runtime inputs are defined by ../recurrent_kda.json.
-#include "aclnn_recurrent_kda.h"
-
-namespace {
-[[maybe_unused]] auto *const kGetWorkspace = &aclnnRecurrentKdaGetWorkspaceSize;
-[[maybe_unused]] auto *const kRun = &aclnnRecurrentKda;
-} // namespace
